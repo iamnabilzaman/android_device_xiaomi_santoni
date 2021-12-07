7107368aea0a32c8aa2935e715628900029555da
iamnabilzaman <94600437+iamnabilzaman@users.noreply.github.com>
2021-12-07 13:46:18 +0600

    Update lineage_santoni.mk

--- lineage_santoni.mk
+++ lineage_santoni.mk
@@ -19,34 +19,12 @@ $(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
 # Inherit some common Lineage OS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
-
-# Touch HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.xiaomi_8937
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 PRODUCT_DEVICE := santoni
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4X
 PRODUCT_NAME := lineage_santoni
 BOARD_VENDOR := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
-
- CipherOS Stuff
-TARGET_FACE_UNLOCK_SUPPORTED := true
-
-# Maintainer
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.cipher.maintainer=iamnabilzaman
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 720
